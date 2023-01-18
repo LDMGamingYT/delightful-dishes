a1457d88878eacca0daa9df52737cf211570dc91
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-01-18 17:32:06 -0500

    Uncommented classes

--- src/main/java/net/ldm/mo_food/block/SweetPotatoesBlock.java
+++ src/main/java/net/ldm/mo_food/block/SweetPotatoesBlock.java
@@ -1,4 +1,4 @@
-/*package net.ldm.mo_food.block;
+package net.ldm.mo_food.block;
 
 import net.ldm.mo_food.init.MoFood;
 import net.minecraft.block.PotatoesBlock;
@@ -15,4 +15,3 @@ public class SweetPotatoesBlock extends PotatoesBlock {
         return MoFood.SWEET_POTATO;
     }
 }
-*/
