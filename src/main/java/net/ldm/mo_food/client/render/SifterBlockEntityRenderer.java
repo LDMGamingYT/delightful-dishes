a21619d020814e1e3533996858f56f0ff190cbd9
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-02-10 18:15:38 -0500

    Sifter BER mostly works

--- src/main/java/net/ldm/mo_food/client/render/SifterBlockEntityRenderer.java
+++ src/main/java/net/ldm/mo_food/client/render/SifterBlockEntityRenderer.java
@@ -2,7 +2,7 @@ package net.ldm.mo_food.client.render;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.ldm.mo_food.block.SifterBlockEntity;
+import net.ldm.mo_food.block.entity.SifterBlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -12,15 +12,14 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.World;
 
 @Environment(EnvType.CLIENT)
 public class SifterBlockEntityRenderer implements BlockEntityRenderer<SifterBlockEntity> {
-    private static ItemStack item = ItemStack.EMPTY;
+    private ItemStack item = ItemStack.EMPTY;
 
-    public SifterBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {}
+    public SifterBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
+    }
 
     public void updateItem(SifterBlockEntity blockEntity) {
         item = blockEntity.getStack(0);
@@ -28,22 +27,24 @@ public class SifterBlockEntityRenderer implements BlockEntityRenderer<SifterBloc
 
     //TODO: Fix item not disappearing after being removed (possible client desync?)
     @Override
-    public void render( SifterBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay ) {
+    public void render(SifterBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         updateItem(blockEntity);
-        System.out.println(item+": "+!(item.isEmpty() || item.isOf(Items.AIR)));
-
         matrices.push();
 
         World world = blockEntity.getWorld();
         assert world != null;
 
-        if (!(item.isEmpty() || item.isOf(Items.AIR))) {
-            double offset = Math.sin((world.getTime() + tickDelta) / 8.0) / 4.0; // Calculate the current offset in the y value
-            matrices.translate(0.5, 1.25 + offset, 0.5); // Move the item
-            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((world.getTime() + tickDelta) * 4)); // Rotate the item
+        float offset = ((float) (blockEntity.getWorld().getTime() - blockEntity.animationStartTime) + tickDelta) / 150f; // Calculate the current offset in the y value
+
+        if (offset >= 0.3f) {
+            MinecraftClient.getInstance().getItemRenderer().renderItem(ItemStack.EMPTY, ModelTransformation.Mode.GROUND,
+                    light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
+        } else {
+            matrices.translate(0.5, 0.7 - offset, 0.5); // move the item to the center, and follow the "offset"
 
             int lightAbove = WorldRenderer.getLightmapCoordinates(blockEntity.getWorld(), blockEntity.getPos().up());
-            MinecraftClient.getInstance().getItemRenderer().renderItem(item, ModelTransformation.Mode.GROUND, lightAbove, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
+            MinecraftClient.getInstance().getItemRenderer().renderItem(item, ModelTransformation.Mode.GROUND, lightAbove,
+                    OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
         }
 
         matrices.pop();
