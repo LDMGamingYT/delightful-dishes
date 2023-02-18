937bb7942f823f15b4250c85f4c035bac1345eb6
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-02-17 22:11:31 -0500

    Moved lettuce, tomatoes, sweet potatoes into basic crop

---
+++ src/main/java/net/ldm/mo_food/block/crop/BasicCrops.java
@@ -0,0 +1,25 @@
+package net.ldm.mo_food.block.crop;
+
+import net.ldm.mo_food.core.init.MoFood;
+
+public class BasicCrops {
+    public static class Garlic extends BasicCrop {
+        public Garlic(Settings settings ) {
+            super(settings, MoFood.GARLIC);
+        }
+    }
+
+    public static class Lettuce extends BasicCrop {
+
+        public Lettuce(Settings settings ) {
+            super(settings, MoFood.LETTUCE);
+        }
+    }
+
+    public static class SweetPotatoes extends BasicCrop {
+
+        public SweetPotatoes(Settings settings ) {
+            super(settings, MoFood.SWEET_POTATO);
+        }
+    }
+}
