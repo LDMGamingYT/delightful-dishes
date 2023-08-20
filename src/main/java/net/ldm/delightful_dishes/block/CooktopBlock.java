abc519514d2e9f72231d1bffb7e26d1376a66b89
Logan Dhillon <logandhi64@gmail.com>
2023-08-20 18:24:01 -0400

    Created default blockstates

--- src/main/java/net/ldm/delightful_dishes/block/CooktopBlock.java
+++ src/main/java/net/ldm/delightful_dishes/block/CooktopBlock.java
@@ -3,6 +3,9 @@ package net.ldm.delightful_dishes.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -10,9 +13,18 @@ import net.minecraft.world.BlockView;
 
 public class CooktopBlock extends Block {
     private static final VoxelShape OUTLINE;
+    public static final IntProperty PANS = IntProperty.of("pans", 0, 4);
+    public static final BooleanProperty LIT = BooleanProperty.of("lit");
 
     public CooktopBlock(Settings settings ) {
         super(settings);
+        setDefaultState(getDefaultState().with(PANS, 0).with(LIT, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(PANS);
+        builder.add(LIT);
     }
 
     @SuppressWarnings("deprecation")
