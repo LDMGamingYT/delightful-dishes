474b6d358a4e0367886b15a6301d76b9033fee4a
Logan Dhillon <logandhi64@gmail.com>
2023-08-20 18:27:44 -0400

    Impl. blockstate test

--- src/main/java/net/ldm/delightful_dishes/block/CooktopBlock.java
+++ src/main/java/net/ldm/delightful_dishes/block/CooktopBlock.java
@@ -3,14 +3,21 @@ package net.ldm.delightful_dishes.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
+@SuppressWarnings("deprecation")
 public class CooktopBlock extends Block {
     private static final VoxelShape OUTLINE;
     public static final IntProperty PANS = IntProperty.of("pans", 0, 4);
@@ -27,7 +34,15 @@ public class CooktopBlock extends Block {
         builder.add(LIT);
     }
 
-    @SuppressWarnings("deprecation")
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (state.get(LIT)) return ActionResult.SUCCESS;
+        player.playSound(SoundEvents.ITEM_FIRECHARGE_USE, 1, 1);
+        world.setBlockState(pos, state.with(LIT, true));
+        return ActionResult.SUCCESS;
+    }
+
     @Override
     public VoxelShape getOutlineShape( BlockState state, BlockView world, BlockPos pos, ShapeContext context ) {
         return OUTLINE;
